e928db9cb1a691a1de2ca5cf017eed2969389583
JuhyoungYoo <josh.jh.yoo@gmail.com>
2020-02-12 17:09:41 +0100

    [Add] - Add detail page

--- bookstore/src/main/java/com/example/bookstore/service/BookService.java
+++ bookstore/src/main/java/com/example/bookstore/service/BookService.java
@@ -1,6 +1,8 @@
 package com.example.bookstore.service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import javax.transaction.Transactional;
 
@@ -11,7 +13,9 @@ import com.example.bookstore.model.entitiy.Book;
 import org.springframework.stereotype.Service;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Service
 @AllArgsConstructor
 public class BookService {
@@ -23,8 +27,55 @@ public class BookService {
         return bookRepository.save(bookDto.toEntity()).getIdx();
     }
 
-    public List<Book> getList() {
-        return bookRepository.findAll();
+    @Transactional
+    public BookDto get(Long idx) {
+        BookDto bookDto = new BookDto();
+        Optional<Book> result = bookRepository.findById(idx);
+        if(result.isPresent()){
+            Book book = result.get();
+            bookDto = BookDto.builder()
+                .idx(book.getIdx())
+                .id(book.getId())
+                .title(book.getTitle())
+                .ISBN(book.getISBN())
+                .author(book.getAuthor())
+                .categories(book.getCategories())
+                .createdDate(book.getCreatedDate())
+                .modifiedDate(book.getModifiedDate())
+                .build();
+        }
+        return bookDto;
+    }
+
+    @Transactional
+    public Long update(BookDto bookDto){
+        return bookRepository.save(bookDto.toEntity()).getIdx();
+    }
+
+    public List<BookDto> getList() {
+        log.info("getList called");
+        List<Book> bookList = bookRepository.findAll();
+        log.info("bookList :: " + bookList);
+
+        List<BookDto> bookDtoList = new ArrayList<>();
+
+        for(Book book : bookList){
+            BookDto bookDto = BookDto.builder()
+                .idx(book.getIdx())
+                .id(book.getId())
+                .title(book.getTitle())
+                .ISBN(book.getISBN())
+                .author(book.getAuthor())
+                .categories(book.getCategories())
+                .createdDate(book.getCreatedDate())
+                .modifiedDate(book.getModifiedDate())
+                .build();
+
+            bookDtoList.add(bookDto);
+        }
+
+        return bookDtoList;
+
     }
 
 }
